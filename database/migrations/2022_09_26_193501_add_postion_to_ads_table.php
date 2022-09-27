8d1e2665d2a7dd15b955dae42545f7a9233b0f7d
Uno negocios <desarrollo@unonegocios.com>
2022-09-26 20:01:17 -0500

    fixed error at name

--- database/migrations/2022_09_26_193501_add_postion_to_ads_table.php
+++ database/migrations/2022_09_26_193501_add_postion_to_ads_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('ads', function (Blueprint $table) {
-            $table->strig('position')->nullable();
+            $table->string('position')->nullable();
         });
     }
 
