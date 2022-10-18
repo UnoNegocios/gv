e114a7de73322942d2da0c7e8b630290e490e74e
Uno negocios <desarrollo@unonegocios.com>
2022-10-18 12:24:55 -0500

    added ad_campaign_id to model request

--- app/Http/Requests/ad/StoreAdRequest.php
+++ app/Http/Requests/ad/StoreAdRequest.php
@@ -38,7 +38,7 @@ class StoreAdRequest extends FormRequest
             'height' => '',
             'width' => '',
             'position' => '',
-            'ad_campaign_id' => ''
+            'ad_campaing_id' => ''
         ];
     }
 }
